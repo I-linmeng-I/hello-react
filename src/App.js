a9adbc68c713fab37f9484ad9d55936617669a08
WIN-OJ111I7NGPD\linmeng <1364876516@qq.com>
2023-04-01 15:30:12 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,5 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0"
+import * as SupabaseSupabaseJs from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0";
+const supabaseUrl = 'https://eggtdvvjoksncmdbxgsz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZ3RkdnZqb2tzbmNtZGJ4Z3N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNjE4MDgsImV4cCI6MTk5MzczNzgwOH0.1PNSCvjCrAqMEUPXpPX6qwZCqTRexjWZuhIzSZeorK0'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+function Testbutton() {
+  const handleClick = () => {
+    console.log('嘻嘻');
+  };
+
+  let text = "balabala";
+
+  return(
+    <button onClick={handleClick}>{text}</button>
+  );
+}
+function Test2(){
+  let a = "还没按"
+  if(true){
+    a="答案不对"
+  }
+  return(
+    <div>{a}</div>
+  );
+}
+
+function Table() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data: testList, error } = await supabase.from('testList').select('*');
+
+      if (error) {
+        console.error(error);
+      } else {
+        setData(testList);
+      }
+    }
+
+    fetchData();
+  }, []);
+  return(
+    <div>
+      <table>
+      <thead>  
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>ISBN</th>
+          <th>description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row)=>(
+          <tr>
+            <td>{row.id}</td>
+            <td>{row.title}</td>
+            <td>{row.author}</td>
+            <td>{row.isbn}</td>
+            <td>{row.description}</td>
+          </tr>
+        )
+        )}
+      </tbody>
+    </table>
+    <script type="module" src="supsbasetable.js"></script>
+  </div>
+  );
+}
 
 function App() {
   return (
@@ -17,6 +92,10 @@ function App() {
         >
           Learn React
         </a>
+        
+        <Testbutton/>
+        <Table/>
+        <Test2/>
       </header>
     </div>
   );
