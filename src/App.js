8b5393b57200ac7cd77a7a8887e59911b71a8819
WIN-OJ111I7NGPD\linmeng <1364876516@qq.com>
2023-05-02 22:06:58 +0800

    1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0"
-import * as SupabaseSupabaseJs from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0";
+const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = 'https://eggtdvvjoksncmdbxgsz.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZ3RkdnZqb2tzbmNtZGJ4Z3N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNjE4MDgsImV4cCI6MTk5MzczNzgwOH0.1PNSCvjCrAqMEUPXpPX6qwZCqTRexjWZuhIzSZeorK0'
 const supabase = createClient(supabaseUrl, supabaseKey)
